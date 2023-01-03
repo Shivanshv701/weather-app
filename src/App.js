6a9e811273a5c7f8d617212a2b72d96e29a13b09
Shivanshv701 <85641493+Shivanshv701@users.noreply.github.com>
2023-01-03 17:05:43 +0530

    Cric2

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
 
 return(
 <div>
-<iframe src="//stream.crichd.vip/update/star.php" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>
+<iframe src="//stream.crichd.vip/update/willowcricket.php" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>
 </div>
 );
 }
