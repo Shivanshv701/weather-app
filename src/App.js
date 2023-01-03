b227aac6020adc2b8debc41dd131115d25b8b45f
Shivanshv701 <85641493+Shivanshv701@users.noreply.github.com>
2023-01-03 16:52:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import './App.css';
 function App() {
+
+<iframe src="//stream.crichd.vip/update/star.php" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  
+allow="encrypted-media"></iframe>
+
+
   const [weather, setWeather] = React.useState('');
   React.useEffect(() => {
     if (navigator.geolocation) {
